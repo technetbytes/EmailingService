4234db63d6b107d79f5b3e22fe947889d7505126
Saqib Ullah <saqib.ullah@markematicsdigital.com>
2022-01-27 22:54:41 +0500

    initial commit

---
+++ mailservice.py
@@ -0,0 +1,42 @@
+import smtplib , ssl
+import yaml
+
+def config_sections():
+    with open("config.yml", "r") as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+    for section in cfg:
+        print(section)
+
+def get_configuration():
+    with open("config.yml","r") as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+    return cfg
+
+def email(cfg, receiver_email):
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    message = """\
+        Subject: Hi there
+
+        This message is sent from Python."""
+
+    # Try to log in to server and send email
+    try:
+        sender_email = cfg['smtp_server']['user']
+        sender_pass = cfg['smtp_server']['passwd']
+        server = smtplib.SMTP_SSL(cfg['smtp_server']['host'], cfg['smtp_server']['port'])
+        server.ehlo() # Can be omitted, To identify yourself to the server
+        server.starttls(context=context) # Secure the connection
+        server.ehlo() # Can be omitted, To identify yourself to the server
+        server.login(sender_email, sender_pass)
+        print("Yes Login ...")
+        server.sendmail(sender_email, receiver_email, message)
+    except Exception as e:
+        # Print any error messages to stdout
+        print("error :- ",e)
+    finally:
+        server.quit() 
+
+
+email(get_configuration(),"saqib@abc.com.pk")
